dc5f92044a7238144dc7ec3bab95870c03b35a4e
giakiet2309 <giakhiemton@gmail.com>
2023-10-11 15:23:35 +0700

    test

---
+++ ss01_csdl/baitap/student_management.sql
@@ -0,0 +1,19 @@
+create database student_management;
+
+use student_management;
+
+create table class(
+ class_id int primary key auto_increment not null,
+ class_name varchar(200)
+);
+
+create table teacher(
+teacher_id int primary key auto_increment  ,
+teacher_name varchar(200),
+teacher_age int,
+teacher_country varchar(200)
+);
+insert into class (class_name) values("kiet");
+insert into teacher(teacher_name,teacher_age,teacher_country) values("kiet",23,"Lào");
+insert into teacher(teacher_name,teacher_age,teacher_country) values("Long",21,"Mỹ");
+insert into teacher(teacher_name,teacher_age,teacher_country) values("Giang",26,"VN");
